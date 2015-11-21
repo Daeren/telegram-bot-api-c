d6a855f99f57deb8319475e9c7be40148550c89e
Daeren <hardcoregame@gmail.com>
2015-11-21 08:47:44 +0300

    [RO]

---
+++ src/server/createHTTP.js
@@ -0,0 +1,284 @@
+//-----------------------------------------------------
+//
+// Author: Daeren
+// Site: 666.io
+//
+//-----------------------------------------------------
+
+"use strict";
+
+//-----------------------------------------------------
+
+const rHttp         = require("http"),
+      rHttps        = require("https");
+
+const rCreateBot    = require("./createBot"),
+      rOnMsg        = require("./onMsg");
+
+//-----------------------------------------------------
+
+module.exports = main;
+
+//-----------------------------------------------------
+
+function main(botFather, params, callback) {
+    if(typeof(params) === "function") {
+        callback = params;
+        params = undefined;
+    }
+
+    params = params || {
+        "host": "127.0.0.1",
+        "http": true
+    };
+
+    params.port = params.port || 88;
+
+    //-----------------]>
+
+    const isHTTPS       = !params.http;
+    const srvBotDefault = rCreateBot(botFather, callback);
+
+    let srv, srvBots;
+
+    //----------)>
+
+    if(isHTTPS) {
+        const certDir = params.certDir || "";
+
+        const optKey  = rFs.readFileSync(certDir + params.key),
+              optCert = rFs.readFileSync(certDir + params.cert);
+
+        let optCa     = params.ca;
+
+        //------)>
+
+        if(optCa) {
+            if(Array.isArray(optCa)) {
+                optCa = optCa.map(function(e) {
+                    return rFs.readFileSync(certDir + e);
+                });
+            }
+            else if(typeof(optCa) === "string") {
+                optCa = certDir + optCa;
+            }
+        }
+
+        //------)>
+
+        const options = {
+            "key":    optKey,
+            "cert":   optCert,
+            "ca":     optCa,
+
+            "ciphers": [
+                "ECDHE-RSA-AES256-SHA384",
+                "DHE-RSA-AES256-SHA384",
+                "ECDHE-RSA-AES256-SHA256",
+                "DHE-RSA-AES256-SHA256",
+                "ECDHE-RSA-AES128-SHA256",
+                "DHE-RSA-AES128-SHA256",
+                "HIGH",
+                "!aNULL",
+                "!eNULL",
+                "!EXPORT",
+                "!DES",
+                "!RC4",
+                "!MD5",
+                "!PSK",
+                "!SRP",
+                "!CAMELLIA"
+            ].join(":"),
+
+            "honorCipherOrder":     true,
+
+            "requestCert":          true,
+            "rejectUnauthorized":   false
+        };
+
+        //---------]>
+
+        srv = rHttps.createServer(options, cbServer);
+    }
+    else {
+        srv = rHttp.createServer(cbServer);
+    }
+
+    srv.listen(params.port, params.host, cbListen);
+
+    //-----------------]>
+
+    return (function() {
+        let result = Object.create(srvBotDefault);
+
+        result.app = srv;
+        result.bot = addBot;
+
+        return result;
+    })();
+
+    //-----------------]>
+
+    function cbServer(req, res) {
+        if(req.method !== "POST") {
+            return response();
+        }
+
+        let firstChunk, chunks;
+
+        //----------]>
+
+        req
+            .on("data", onData)
+            .on("end", onEnd);
+
+        //----------]>
+
+        function onData(chunk) {
+            if(!firstChunk) {
+                firstChunk = chunk;
+            }
+            else {
+                chunks = chunks || [firstChunk];
+                chunks.push(chunk);
+            }
+        }
+
+        function onEnd() {
+            response();
+
+            //--------]>
+
+            const objBot      = srvBots && srvBots[req.url] || srvBotDefault,
+                  cbLogger    = objBot.cbLogger;
+
+            let data    = chunks ? Buffer.concat(chunks) : firstChunk,
+                error   = null;
+
+            //--------]>
+
+            try {
+                data = JSON.parse(data);
+            } catch(e) {
+                error = e;
+            }
+
+            //--------]>
+
+            if(cbLogger) {
+                cbLogger(error, data);
+            }
+
+            if(error) {
+                return;
+            }
+
+            //--------]>
+
+            rOnMsg(objBot, data);
+        }
+
+        //-------)>
+
+        function response(code) {
+            res.writeHead(code || 200);
+            res.end();
+        }
+    }
+
+    function cbListen() {
+        const host = srv.address().address,
+              port = srv.address().port;
+
+        //-------]>
+
+        onAction(null, "START");
+
+        //-------]>
+
+        process.on("SIGINT", onAction);
+        process.on("uncaughtException", onAction);
+
+        //-------]>
+
+        function onAction(error, evName) {
+            evName = evName || "STOP";
+
+            console.log("\n+---[%s]------------------------------------".slice(0, -1 * evName.length), evName);
+            console.log("|");
+            console.log("| Server: [%s]", getAddress());
+            console.log("| Date: %s", getTime());
+            console.log("+-----------------------------------------\n");
+
+            if(error) {
+                console.error(error.stack);
+            }
+
+            if(evName === "STOP")
+                process.exit();
+        }
+
+        //---)>
+
+        function getAddress() {
+            return (params.http ? "http" : "https") + "://" + (host || "*") + ":" + port;
+        }
+
+        function getTime() {
+            return new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+        }
+    }
+
+    //-----------------]>
+
+    function addBot(bot, path, callback) {
+        srvBots = srvBots || {};
+
+        if(hasOwnProperty(srvBots, path)) {
+            throw new Error("Path '" + path + "' has already been used");
+        }
+
+        //-------------]>
+
+        const srvBot = srvBots[path] = rCreateBot(bot, callback);
+
+        if(typeof(params.autoWebhook) === "undefined" || typeof(params.autoWebhook) === "string" && params.autoWebhook) {
+            if(params.autoWebhook || params.host) {
+                const url = (params.autoWebhook || (params.host + ":" + params.port)) + path;
+                const data = {"url": url, "certificate": params.selfSigned};
+
+                srvBot
+                    .bot
+                    .callJson("setWebhook", data, function(error, isOk) {
+                        if(isOk) {
+                            return;
+                        }
+
+                        console.log("[-] Webhook: %s", url);
+
+                        if(error) {
+                            if(error.message) {
+                                console.log(error.message);
+                            }
+
+                            console.log(error.stack);
+                        }
+                    }
+                );
+            }
+            else {
+                console.log("[!] Warning | `autoWebhook` and `host` not specified, autoWebhook not working");
+            }
+        }
+
+        //-------------]>
+
+        return srvBot;
+    }
+}
+
+//----------------------------------------]>
+
+function hasOwnProperty(obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+}
---
+++ src/server/onMsg.js
@@ -0,0 +1,284 @@
+//-----------------------------------------------------
+//
+// Author: Daeren
+// Site: 666.io
+//
+//-----------------------------------------------------
+
+"use strict";
+
+//-----------------------------------------------------
+
+const rParseCmd         = require("./../parseCmd");
+
+const CResponseBuilder  = require("./responseBuilder");
+
+//-----------------------------------------------------
+
+module.exports = main;
+
+//-----------------------------------------------------
+
+function main(objBot, data) {
+    const msg     = data.message,
+          msgChat = msg.chat;
+
+    //--------]>
+
+    const botPlugin       = objBot.plugin,
+          botFilters      = objBot.filters,
+
+          ctxBot          = createCtx(),
+
+          msgType         = getTypeMsg(msg),
+          evName          = getEventNameByTypeMsg(msgType);
+
+    let cmdParam;
+
+    //------------]>
+
+    forEachAsync(botPlugin, onIterPlugin, onEndPlugin);
+
+    //------------]>
+
+    function onIterPlugin(next, plugin) {
+        const plType        = plugin[0],
+              plCallback    = plugin[1];
+
+        let isEnd = false;
+
+        //---------]>
+
+        if(typeof(plType) !== "undefined") {
+            if(evName !== plType) {
+                onEnd();
+            }
+            else {
+                if(plCallback.length < 2) {
+                    onEnd(plCallback(ctxBot));
+                }
+                else {
+                    plCallback(ctxBot, onEnd);
+                }
+            }
+
+            return;
+        }
+
+        if(plCallback.length < 3) {
+            onEnd(plCallback(evName, ctxBot));
+        }  else {
+            plCallback(evName, ctxBot, onEnd);
+        }
+
+        //---------]>
+
+        function onEnd(state) {
+            if(isEnd) {
+                throw new Error("Plugin: double call `next`");
+            }
+
+            isEnd = true;
+
+            setImmediate(next, state);
+        }
+    }
+
+    function onEndPlugin(state) {
+        switch(evName) {
+            case "text":
+                let rule, len;
+
+                //-----[Filter: botName]----}>
+
+                if(!msg.reply_to_message && msgChat.id < 0 && msgChat.type === "group") {
+                    let msgText = msg.text;
+
+                    if(msgText[0] === "@") {
+                        msg.text = msgText.replace(gReReplaceName, "");
+                    }
+                }
+
+                //-----[CMD]----}>
+
+                cmdParam = rParseCmd(msg.text);
+
+                if(cmdParam) {
+                    rule = "/" + cmdParam.name;
+
+                    if(callEvent(rule, cmdParam) || callEvent("/", cmdParam)) {
+                        return;
+                    }
+                }
+
+                //-----[RE]----}>
+
+                len = botFilters.regexp.length;
+
+                if(len) {
+                    let reParams;
+
+                    rule = undefined;
+
+                    for(let re, i = 0; !rule && i < len; i++) {
+                        re = botFilters.regexp[i];
+                        reParams = msg.text.match(re.rule);
+
+                        if(reParams) {
+                            rule = re.rule;
+
+                            if(rule && re.binds) {
+                                let result  = {},
+                                    binds   = re.binds;
+
+                                for(let j = 0, jLen = Math.min(reParams.length - 1, binds.length); j < jLen; j++) {
+                                    result[binds[j]] = reParams[j + 1];
+                                }
+
+                                reParams = result;
+                            }
+                        }
+                    }
+
+                    if(rule) {
+                        botFilters.ev.emit(rule, ctxBot, reParams);
+                        return;
+                    }
+                }
+
+                break;
+        }
+
+        if(!evName || !callEvent(evName, msg[msgType]) && !callEvent("*", cmdParam)) {
+            if(objBot.onMsg) {
+                setImmediate(objBot.onMsg, ctxBot, cmdParam);
+            }
+        }
+
+        //-------]>
+
+        function callEvent(type, params) {
+            if(state) {
+                type += ":" + state;
+            }
+
+            if(botFilters.ev.listenerCount(type)) {
+                botFilters.ev.emit(type, ctxBot, params);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    //-------)>
+
+    function createCtx() {
+        let result = Object.create(objBot.ctx);
+
+        result.from = result.cid = msgChat.id;
+        result.mid = msg.message_id;
+
+        result.message = msg;
+        result.data = createFResponseBuilder();
+
+        result.createFResponseBuilder = createFResponseBuilder;
+
+        //---------------]>
+
+        return result;
+
+        //---------------]>
+
+        function createFResponseBuilder() {
+            return function() { return new CResponseBuilder(result, objBot.bot); };
+        }
+    }
+}
+
+//----------------------------------------]>
+
+function getEventNameByTypeMsg(type) {
+    switch(type) {
+        case "new_chat_participant":    return "enterChat";
+        case "left_chat_participant":   return "leftChat";
+
+        case "new_chat_title":          return "chatTitle";
+        case "new_chat_photo":          return "chatNewPhoto";
+        case "delete_chat_photo":       return "chatDeletePhoto";
+        case "group_chat_created":      return "chatCreated";
+    }
+
+    return type;
+}
+
+function getTypeMsg(m) {
+    let t;
+
+    if(
+        hasOwnProperty(m, t = "text") ||
+        hasOwnProperty(m, t = "photo") ||
+        hasOwnProperty(m, t = "audio") ||
+        hasOwnProperty(m, t = "document") ||
+        hasOwnProperty(m, t = "sticker") ||
+        hasOwnProperty(m, t = "video") ||
+        hasOwnProperty(m, t = "voice") ||
+        hasOwnProperty(m, t = "contact") ||
+        hasOwnProperty(m, t = "location") ||
+
+        hasOwnProperty(m, t = "new_chat_participant") ||
+        hasOwnProperty(m, t = "left_chat_participant") ||
+        hasOwnProperty(m, t = "new_chat_title") ||
+        hasOwnProperty(m, t = "new_chat_photo") ||
+        hasOwnProperty(m, t = "delete_chat_photo") ||
+        hasOwnProperty(m, t = "group_chat_created")
+    ) return t;
+}
+
+//---------]>
+
+function hasOwnProperty(obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+function forEachAsync(data, iter, cbEnd) {
+    let i   = 0,
+        len = data.length;
+
+    //---------]>
+
+    if(len) {
+        run();
+    }
+    else {
+        if(cbEnd) {
+            cbEnd();
+        }
+    }
+
+    //---------]>
+
+    function run() {
+        iter(cbNext, data[i], i);
+    }
+
+    function cbNext(error, result) {
+        if(error) {
+            if(cbEnd) {
+                cbEnd(error);
+            }
+
+            return;
+        }
+
+        i++;
+
+        if(i >= len) {
+            if(cbEnd) {
+                cbEnd(error, result);
+            }
+        } else {
+            run();
+        }
+    }
+}
